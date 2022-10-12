f7246169f1fe80053e4f61a241a2f3077cd74a10
Susanna Peretti <susa.peretti@gmail.com>
2022-10-12 20:29:30 +0200

    login view

--- src/Controllers/LoginViewController.java
+++ src/Controllers/LoginViewController.java
@@ -6,14 +6,19 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author Mars_DB
+ * @author susy9
  */
 public class LoginViewController implements Initializable {
+
+    @FXML
+    private TextField sign_up_username;
 
     /**
      * Initializes the controller class.
---
+++ src/Controllers/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+/**
+ *
+ * @author susy9
+ */
+public class NewClass {
+    
+}
